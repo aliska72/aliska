47891ab776aae664c8e441d15909eb4b4501dc3e
aliska72 <vipalisarepina635@gmail.com>
2026-05-04 12:52:53 +0500

    3 commit

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -193,28 +193,27 @@
           <li class="nav-item">
             <a href="{{ route('admin.index') }}" class="nav-link">
               <i class="nav-icon fas fa-home"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
+              <p>Главная</p>
             </a>
-            <ul class="nav nav-treeview">
+          </li>
+              <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-archive"></i>
+                  <p>Категории
+                      <i class="right fas fa-angel-left"></i>
+                  </p>
+                </a>
+              <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="../../index.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
+                <a href="{{ route('categories.index') }}" class="nav-link">
+                  <i class="far fa-c)ircle nav-icon"></i>
+                  <p>Список категорий</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="../../index2.html" class="nav-link">
+                <a href="{{ route('categories.create') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../../index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
+                  <p>Новая категория</p>
                 </a>
               </li>
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Route::group(['prefix'=> 'admin', 'namespace' => 'Admin'], function () {
         Route::get('/', [MainController::class, 'index']) ->name('admin.index');
 });
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function (){
+    Route::resource('/categories', 'CategoryController');
+});
